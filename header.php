e15f248f8b12f247cc7bc52f69df722f40543d72
yukino-nakamaru <y.nakamaru23@gmail.com>
2024-01-05 19:36:46 +0900

    スペル修正

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>PHP Sample Programs</titke>
+<title>PHP Sample Programs</title>
 <link rel="stylesheet" href="../style.css">
 <link rel="stylesheet" href="../style.css">
 <link rel="stylesheet" href="style.css">
